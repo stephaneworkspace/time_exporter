aa5f6cdb1c5e117794f30e36e8ab5d48d2e7334c
Stéphane Bressani <stephane@bressani.dev>
2025-05-08 13:08:52 +0200

    amélioration plusieurs page excel

--- time_exporter.rb
+++ time_exporter.rb
@@ -14,59 +14,60 @@ response = HTTParty.get(
 )
 
 if response.code == 200
-  sessions = JSON.parse(response.body).select { |s| s["project_id"] == 5 }
+  sessions_by_project = JSON.parse(response.body).group_by { |s| s.dig("project", "name") }
 
   Axlsx::Package.new do |p|
-    p.workbook.add_worksheet(name: '5') do |sheet|
-      styles = sheet.workbook.styles
-      datetime_bordered_style = styles.add_style(
-        format_code: "dd/mm/yyyy hh:mm:ss",
-        border: { style: :thin, color: "000000", edges: [:top, :bottom, :left, :right] }
-      )
-      decimal_bordered_style = styles.add_style(
-        format_code: "0.00",
-        border: { style: :thin, color: "000000", edges: [:top, :bottom, :left, :right] }
-      )
-      bordered_style = styles.add_style(
-        border: { style: :thin, color: "000000", edges: [:top, :bottom, :left, :right] }
-      )
-      header_style = styles.add_style(
-        b: true,
-        border: {
-          style: :medium,
-          color: "000000",
-          edges: [:top, :bottom, :left, :right]
-        },
-        alignment: { horizontal: :center }
-      )
+    sessions_by_project.each do |project_name, sessions|
+      p.workbook.add_worksheet(name: project_name[0..30]) do |sheet|
+        styles = sheet.workbook.styles
+        datetime_bordered_style = styles.add_style(
+          format_code: "dd/mm/yyyy hh:mm:ss",
+          border: { style: :thin, color: "000000", edges: [:top, :bottom, :left, :right] }
+        )
+        decimal_bordered_style = styles.add_style(
+          format_code: "0.00",
+          border: { style: :thin, color: "000000", edges: [:top, :bottom, :left, :right] }
+        )
+        bordered_style = styles.add_style(
+          border: { style: :thin, color: "000000", edges: [:top, :bottom, :left, :right] }
+        )
+        header_style = styles.add_style(
+          b: true,
+          border: {
+            style: :medium,
+            color: "000000",
+            edges: [:top, :bottom, :left, :right]
+          },
+          alignment: { horizontal: :center }
+        )
 
-      sheet.add_row ["ID", "Début", "Fin", "Durée", "Jour travail", "Commentaire"], style: [header_style] * 6
+        sheet.add_row ["ID", "Début", "Fin", "Durée", "Jour travail", "Commentaire"], style: [header_style] * 6
 
-      sessions.each do |s|
-        started_at = Time.parse(s["started_at"])
-        ended_at = Time.parse(s["ended_at"])
-        duration_seconds = ended_at - started_at
-        duration = Time.at(duration_seconds).utc.strftime("%H:%M:%S")
-        sheet.add_row [
-          s["id"],
-          started_at,
-          ended_at,
-          duration,
-          nil,  # Placeholder for formula
-          s["commentaire"]
-        ], style: [bordered_style, datetime_bordered_style, datetime_bordered_style, bordered_style, decimal_bordered_style, bordered_style]
+        sessions.each do |s|
+          started_at = Time.parse(s["started_at"])
+          ended_at = Time.parse(s["ended_at"])
+          duration_seconds = ended_at - started_at
+          duration = Time.at(duration_seconds).utc.strftime("%H:%M:%S")
+          sheet.add_row [
+            s["id"],
+            started_at,
+            ended_at,
+            duration,
+            nil,
+            s["commentaire"]
+          ], style: [bordered_style, datetime_bordered_style, datetime_bordered_style, bordered_style, decimal_bordered_style, bordered_style]
 
-        last_row_index = sheet.rows.size
-        sheet.rows.last.cells[4].value = "=(C#{last_row_index}-B#{last_row_index})*24/8.5"
+          last_row_index = sheet.rows.size
+          sheet.rows.last.cells[4].value = "=(C#{last_row_index}-B#{last_row_index})*24/8.5"
+        end
+
+        total_row_index = sheet.rows.size + 1
+        sheet.add_row ["", "", "", "", nil, "Total jours"], style: [bordered_style]*6
+        sheet.rows.last.cells[4].value = "=SUM(E2:E#{total_row_index - 1})"
+
+        sheet.add_row ["", "", "", "", nil, "Total heures"], style: [bordered_style]*6
+        sheet.rows.last.cells[4].value = "=E#{total_row_index}*8.5"
       end
-
-      # Add total rows
-      total_row_index = sheet.rows.size + 1
-      sheet.add_row ["", "", "", "", nil, "Total jours"], style: [bordered_style]*6
-      sheet.rows.last.cells[4].value = "=SUM(E2:E#{total_row_index - 1})"
-
-      sheet.add_row ["", "", "", "", nil, "Total heures"], style: [bordered_style]*6
-      sheet.rows.last.cells[4].value = "=E#{total_row_index}*8.5"
     end
     p.serialize("sessions.xlsx")
   end
