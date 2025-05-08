917024dc4a0a16fb271e193461a92fffb475a334
Stéphane Bressani <stephane@bressani.dev>
2025-05-08 11:39:24 +0200

    ok

--- time_exporter.rb
+++ time_exporter.rb
@@ -18,18 +18,23 @@ if response.code == 200
 
   Axlsx::Package.new do |p|
     p.workbook.add_worksheet(name: '5') do |sheet|
-      sheet.add_row ["ID", "Début", "Fin", "Commentaire"]
+      sheet.add_row ["ID", "Début", "Fin", "Durée", "Commentaire"]
 
       styles = sheet.workbook.styles
       datetime_style = styles.add_style(format_code: "dd/mm/yyyy hh:mm:ss")
 
       sessions.each do |s|
+        started_at = Time.parse(s["started_at"])
+        ended_at = Time.parse(s["ended_at"])
+        duration_seconds = ended_at - started_at
+        duration = Time.at(duration_seconds).utc.strftime("%H:%M:%S")
         sheet.add_row [
-                        s["id"],
-                        Time.parse(s["started_at"]),
-                        Time.parse(s["ended_at"]),
-                        s["commentaire"]
-                      ], style: [nil, datetime_style, datetime_style, nil]
+          s["id"],
+          started_at,
+          ended_at,
+          duration,
+          s["commentaire"]
+        ], style: [nil, datetime_style, datetime_style, nil, nil]
       end
     end
     p.serialize("sessions.xlsx")
